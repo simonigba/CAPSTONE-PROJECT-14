6cb7d64b8ba5069e091feb358b0d5b36f7e09944
Olakiitan20 <agbajewasiu6@gmail.com>
2026-05-12 17:26:30 +0100

    Just finished the hero and header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./Header.css";
+
+function Header() {
+return (
+<>
+{/* Header */}
+<header className="ader">
+<div className="navbar">
+<a href="/">
+<img
+src="https://res.cloudinary.com/dva4kkwec/image/upload/v1778449959/Logo_gl7yjh.png"
+width="auto"
+height="50px"
+margin-top="20px"
+alt="logo"
+/>
+</a>
+</div>
+</header>
+
+{/* Hero Section */}
+<section className="head">
+<div className="header">
+
+<div className="header-content">
+<h1>
+Explore Our Solar <br /> System Through Data
+</h1>
+
+<p className="p">
+Understand the planets not just by name, but by measurable facts.
+From size and mass to gravity and density, this page breaks down
+the solar system in a clear, data-driven way.
+</p>
+
+  <div class="hero-buttons">
+    <a href="#planet-data" class="btn primary">Explore the Data</a>
+    <a href="#form" class="btn secondary">Contact Us</a>
+ </div>
+</div>
+
+<div className="header-image">
+<img
+src="https://res.cloudinary.com/dva4kkwec/image/upload/v1778449959/ImageFirst_rnfc7s.png"
+alt="Earth Globe"
+/>
+</div>
+
+</div>
+</section>
+</>
+);
+}
+
+export default Header
